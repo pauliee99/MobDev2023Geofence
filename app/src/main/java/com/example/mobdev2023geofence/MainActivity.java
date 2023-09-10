39455d31cc760b6ea2a34006b62d41ba9d4be84f
pauliee99 <pavlosan67@gmail.com>
2023-09-11 01:36:47 +0300

    user session added needs work

--- app/src/main/java/com/example/mobdev2023geofence/MainActivity.java
+++ app/src/main/java/com/example/mobdev2023geofence/MainActivity.java
@@ -5,23 +5,62 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private long sessionStartTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startSession();
 
         // @TODO: maybe add some progress bars around here ( MobDev (2022 12 12) )
         Button btnMapActivity = (Button)findViewById(R.id.buttonMapActivity);
+        Button btnResultsMapActivity = (Button)findViewById(R.id.buttonResultsMapActivity);
         btnMapActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, MapsActivity.class));
             }
         });
+
+        btnResultsMapActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ResultsMapActivity.class));
+            }
+        });
+    }
+
+    private void startSession() {
+        sessionStartTime = System.currentTimeMillis();
+        // Display a welcome message or perform any other actions.
+        Toast.makeText(this, "Welcome! Session started.", Toast.LENGTH_SHORT).show();
+    }
+
+    private void endSession() {
+        long sessionEndTime = System.currentTimeMillis();
+        long sessionDuration = sessionEndTime - sessionStartTime;
+
+        // Record session data, e.g., in a database or log file
+        Log.d("SessionInfo", "Session started at: " + sessionStartTime);
+        Log.d("SessionInfo", "Session ended at: " + sessionEndTime);
+        Log.d("SessionInfo", "Session duration (ms): " + sessionDuration);
+
+        // Display a message to indicate the session has ended
+        Toast.makeText(this, "Session ended.", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // End the session when the app is closed
+        endSession();
     }
 }
---
+++ app/src/main/java/com/example/mobdev2023geofence/ResultsMapActivity.java
@@ -0,0 +1,87 @@
+package com.example.mobdev2023geofence;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.mobdev2023geofence.databinding.ActivityResultsMapBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class ResultsMapActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityResultsMapBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityResultsMapBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 4) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                showPosition();
+            }
+        }
+    }
+
+    private void showPosition(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 4);
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        showPosition();
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        FloatingActionButton cancelButton = findViewById(R.id.cancelButton);
+        cancelButton.setOnClickListener(new View.OnClickListener() { // go back button
+            @Override
+            public void onClick(View view) {
+                //startActivity(new Intent(MapsActivity.this, MainActivity.class));
+                finish();
+            }
+        });
+    }
+}
